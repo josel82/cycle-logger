30bc22bd461e923ef09b1ae86f34e506c3371241
josel82 <jose.padilla.dev@gmail.com>
2018-10-08 13:36:00 +0100

    Restructured Header component. Added Logout feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,26 @@
-import React, { Component } from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-class Header extends Component {
-    constructor(props){
-        super(props);
-        this.getNavigationLink = this.getNavigationLink.bind(this);
-        this.onLogout = this.onLogout.bind(this);
-        this.state = {loggedIn:false};
-    }
-    getNavigationLink(){
-        const {pathname} = this.props.location;
-                
-        if(pathname === '/signin'){
-            return <li><NavLink to={'/signup'} activeClassName="is-active">Sign up</NavLink></li>;
-        }else if(pathname === '/signup'){
-            return <li><NavLink to={'/signin'} activeClassName="is-active">Log in</NavLink></li>;
-        }
-        return;
-    }
-    onLogout(e){
-        e.preventDefault();
-        this.setState({loggedIn:false})
-        this.props.history.push('/');
-    }
-    render(){
-        return (
-            <header className="header">
-                <h2 className="header__logo">Cycle-Logger</h2>
-                <nav className="header__navigation">
-                    <ul> 
-                        {this.getNavigationLink()}   
-                        {this.state.loggedIn && <li><a onClick={this.onLogout}>Log out</a></li>}
-                    </ul>
-                </nav>
-            </header>
-        );
-    }
-}
+const Header = ({ startLogout }) => (
+            
+    <header className="header">
+        <h2 className="header__logo">Cycle-Logger</h2>
+        <nav className="header__navigation">
+            <ul> 
+                <li><NavLink to={'/signup'} >Sign up</NavLink></li> 
+                <li><NavLink to={'/'} >Log in</NavLink></li> 
+                <li><NavLink to={'/'} onClick={startLogout}>Log out</NavLink></li>
+            </ul>
+        </nav>
+    </header>
+);
+ 
 
-export default withRouter(Header);
+
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
