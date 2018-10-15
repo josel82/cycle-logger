69c569bcee659611500cd84ee0daf845c517244f
josel82 <jose.padilla.dev@gmail.com>
2018-10-15 22:48:35 +0100

    implemented responsive design in the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { startLogout } from '../actions/auth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
-const Header = ({ startLogout }) => (
-            
-    <header className="header">
-        <div className="logo-box">
-            <img src="/assets/img/logo-lg-inv.png" alt="Logo" className="logo"/>
-        </div>
-        <nav className="header__navigation">
-            <ul> 
-                <li>
-                    <NavLink to={'/'} onClick={startLogout}>
-                        <span className="nav-text">Log out</span>
-                        <span className="nav-icon">
-                            <FontAwesomeIcon icon={faBars}/>
-                        </span>
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
-);
+import { startLogout } from '../actions/auth';
+
+class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isOpened: false
+        }
+    }
+    toggleMenu = () => {
+        this.setState({isOpened:!this.state.isOpened});
+    }
+    render(){
+        return (         
+            <header className="header">
+                <div className="logo-box">
+                    <img src="/assets/img/logo-lg-inv.png" alt="Logo" className="logo"/>
+                </div>
+                <nav className={`header__navigation ${this.state.isOpened ? 'header__navigation--open':''}`}>
+                    <ul> 
+                        <li>
+                            <NavLink to={'/'} onClick={this.props.startLogout}>
+                                <FontAwesomeIcon icon={faSignOutAlt} className="header__navigation--icon" />Log out
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={`menu-toggle ${this.state.isOpened ? 'open':''}`} 
+                     onClick={this.toggleMenu}>
+                    <div className="hamburger"></div>
+                </div>
+            </header>
+        );
+    }
+}
  
 
 
