ce997b72c5523bf8d065bb3cd2366f9f88049b1e
josel82 <jose.padilla.dev@gmail.com>
2018-10-16 05:41:42 +0100

    Improved LoginPage styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,12 +7,9 @@ const LoginPage = (props) =>
  (
           <div className="login-container">
             <h1 className="heading-primary">
-                <span className="heading-primary__parcial">
-                    <span className="heading-primary__parcial__bold">C</span>ycle
-                </span>
-                <span className="heading-primary__parcial">
-                    <span className="heading-primary__parcial__bold">L</span>ogger
-                </span>
+                <span className="heading-primary__parcial__bold">C</span>ycle
+                <br/>
+                <span className="heading-primary__parcial__bold">L</span>ogger
             </h1>
             <div className="btn btn-google btn-md" onClick={props.startLogin}>
                 <span className="btn-google__logo"></span>
