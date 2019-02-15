5bf8052401af713a2f60c2f3c653a0841976e680
josel82 <jose.padilla.dev@gmail.com>
2019-02-15 19:57:41 +0000

    Refactored Entry component for testing purposes

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import { withRouter } from 'react-router-dom';
+import numeral from 'numeral';
+import '../formats/mg';
 
 
-const Entry = ({history, dispatch, id, compound, quantity, timestamp}) => {
-    const date = moment(timestamp).format('Do MMM, YYYY')
-    return (
-      <tr onClick={(e)=>{
-        history.push(`/dashboard/edit/${id}`);
-      }}>
-        <td><p>{compound}</p></td>
-        <td><p>{quantity}</p></td>
-        <td>
-          <p>{date}</p>
-        </td>
-      </tr>
-    )
+export class Entry extends Component {
+    constructor(props){
+      super(props);
+      this.date = moment(this.props.timestamp).format('Do MMM, YYYY');
+      this.formatedQty = numeral(this.props.quantity).format('0[.]00 mg');
+    }
+    
+    redirectToEditEntryPage = () => {
+      this.props.history.push(`/dashboard/edit/${this.props.id}`);
+    }
+    render(){
+      return (
+        <tr onClick={this.redirectToEditEntryPage}>
+              <td><p>{this.props.compound}</p></td>
+              <td><p>{this.formatedQty}</p></td>
+              <td>
+                <p>{this.date}</p>
+              </td>
+            </tr>
+          )
+        } 
   };
 
   const EntryWithRouter = withRouter(Entry)
