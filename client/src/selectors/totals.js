0b6229b4da53ef30fd24145e322417cb20de37fa
josel82 <jose.padilla.dev@gmail.com>
2019-02-16 12:10:21 +0000

    refactored selector

--- client/src/selectors/totals.js
+++ client/src/selectors/totals.js
@@ -1,16 +1,12 @@
 import moment from 'moment';
 
 export default (entries,  {text, startDate, endDate}) => {
-    if(entries.length<=0){
-        return 0;
-    }else{
-        return entries.filter((entry)=>{
-            const timestampMoment = moment(entry.timestamp);
-            const startDateMatch = startDate ? startDate.isSameOrBefore(timestampMoment, 'day') : true;
-            const endDateMatch = endDate ? endDate.isSameOrAfter(timestampMoment, 'day') : true;
-            const textMatch = entry.compound.toLowerCase().includes(text.toLowerCase())
+    return entries.filter((entry)=>{
+        const timestampMoment = moment(entry.timestamp);
+        const startDateMatch = startDate ? startDate.isSameOrBefore(timestampMoment, 'day') : true;
+        const endDateMatch = endDate ? endDate.isSameOrAfter(timestampMoment, 'day') : true;
+        const textMatch = entry.compound.toLowerCase().includes(text.toLowerCase())
     
-            return startDateMatch && endDateMatch && textMatch;
-        }).reduce((prev, {quantity})=> prev + quantity ,0);
-    }
+        return startDateMatch && endDateMatch && textMatch;
+    }).reduce((prev, {quantity})=> prev + quantity ,0);
 }
